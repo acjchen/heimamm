341abd3a7bcf6e932ba12348365c6009b91856f0
acjchen <1149537331@qq.com>
2020-06-20 21:42:54 +0800

    登录模块-整合路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+//整和路由
+import router from '@/router'
+// 整合Element-ui
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+// use Element-ui
+Vue.use(ElementUI);
 
+
+
+// 导入全局样式
+// import "@/styles/base.less";
+
+
+//是否显示生产环境
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  //挂载到vue实例上
+  router
 }).$mount('#app')
+//挂载到id为app的div上
--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,17 @@
 import Vue from 'vue';
 //导入路由
 import VueRouter from 'vue-router';
+import login from '@/views/login/login.vue'
 //注册路由
 Vue.use(VueRouter);
 //创建路由规则
-// const routes = [];
 //创建路由对象
 const router = new VueRouter({
     routes: [
-        // {
-        //     path:"",
-        //     component:
-        // }
+        {
+            path:'/login',
+            component:login
+        }
     ]
 });
 //暴露出去
