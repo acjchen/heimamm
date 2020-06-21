f073723d11cd0bcbe422bc1ec99f3016e0268262
acjchen <1149537331@qq.com>
2020-06-21 15:33:14 +0800

    登录页面结构样式代码

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,13 +1,161 @@
 <template>
-  <div>登录</div>
+  <div class="login-container">
+    <!-- 左侧表单 -->
+    <div class="left">
+      <div class="title-box">
+        <img src="@/assets/login_icon.png" alt />
+        <span class="title">黑马面面</span>
+        <span class="line"></span>
+        <span class="sub-title">用户登录</span>
+      </div>
+      <!-- 表单 -->
+      <el-form :model="loginForm" :rules="rules" ref="loginForm" class="login-form">
+        <el-form-item prop="phone">
+          <el-input v-model="loginForm.phone" placeholder="请输入手机号" prefix-icon="el-icon-user"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            type="password"
+            v-model="loginForm.password"
+            placeholder="请输入密码"
+            prefix-icon="el-icon-lock"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-col :span="17">
+            <el-input v-model="loginForm.code" placeholder="请输入验证码" prefix-icon="el-icon-key"></el-input>
+          </el-col>
+          <el-col :span="7">
+            <!-- 验证码图片 -->
+            <img :src="actions" class="captcha" @click="randomLoginCaptcha" alt />
+          </el-col>
+        </el-form-item>
+        <!-- 协议复选框 -->
+        <el-form-item class="rule-box" prop="checked">
+          <el-checkbox v-model="loginForm.checked">
+            我已经阅读并同意
+            <el-link type="primary">用户协议</el-link>
+            <span>和</span>
+            <el-link type="primary">隐私条款</el-link>
+          </el-checkbox>
+        </el-form-item>
+        <el-form-item class="btn-box">
+          <el-button class="loginBtn" type="primary" @click="submitForm('loginForm')">登录</el-button>
+          <el-button class="register" type="primary" @click="registerFormVisible = true">注册</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <!-- 右侧图片 -->
+    <div class="right">
+      <img src="../../assets/login_bg.png" alt />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name:"login"
-}
+  name: "login",
+  data() {
+    return {
+      loginForm: {
+        phone: "",
+        password: "",
+        code: "",
+        checked: false
+      },
+      rules: {
+        phone: [
+          { required: true, message: "手机号不能为空" },
+          { validator: checkMobile }
+        ],
+        password: [
+          { required: true, message: "密码不能为空" },
+          { min: 4, max: 12, message: "密码长度为6~12个字符" }
+        ],
+        code: [
+          { required: true, message: "验证码不能为空" },
+          { min: 4, max: 4, message: "验证码长度为4" }
+        ],
+        checked: [{ validator: checkAgree }]
+      },
+      registerFormVisible: false
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          //登录接口
+          login(this.loginForm).then(res => {
+            console.log(res);
+            //保存token
+            setToken(res.data.data.token);
+            //跳转首页
+            this.router.push("/index");
+          });
+        } else {
+         this.$message.warning("请检查输入的内容");
+         return false;
+        }
+      });
+    }
+  }
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.login-container {
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(
+    225deg,
+    rgba(20, 147, 250, 1),
+    rgba(1, 198, 250, 1)
+  );
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  .left {
+    width: 478px;
+    height: 550px;
+    background: rgba(245, 245, 245, 1);
+    padding: 42px;
+    .title-box {
+      display: flex;
+      align-items: center;
+      font-size: 22px;
+      img {
+        width: 22px;
+        height: 17px;
+      }
+      .title {
+        margin-left: 14px;
+      }
+      .line {
+        width: 1px;
+        height: 28px;
+        background: rgba(199, 199, 199, 1);
+        margin-left: 14px;
+        margin-right: 14px;
+      }
+    }
+    .login-form {
+      margin-top: 26px;
+      .el-form-item {
+        margin-bottom: 26px;
+      }
+      .el-button {
+        width: 100%;
+        margin-bottom: 26px;
+      }
+      .register {
+        margin-left: 0;
+      }
+    }
+  }
+  .right {
+    width: 633px;
+    height: 435px;
+  }
+}
 </style>
